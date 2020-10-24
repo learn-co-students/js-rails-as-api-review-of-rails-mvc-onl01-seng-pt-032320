7102f298587f50459aac8f9cef5f2a8d9e8112fe
github username <github email address>
2020-10-24 15:10:59 -0400

    Done.

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,1 +1,12 @@
 # Add code from Readme
+class CreateBirds < ActiveRecord::Migration[5.2]
+    def change 
+#change method = removes the need to write both up and down methods in those cases that Rails know how to revert the changes automatically
+      create_table :birds do |t|
+        t.string :name
+        t.string :species
+   
+        t.timestamps
+      end
+    end
+end
